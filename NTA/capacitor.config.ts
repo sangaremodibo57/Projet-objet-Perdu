e5a80012412104a4dfeaa2ee21794b3873dd1937
Modibo Sangare <sangaremodibo57@gmail.com>
2022-01-14 16:43:14 +0000

    Frontend

--- NTA/capacitor.config.ts
+++ NTA/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'GroupAuto',
+  appName: 'NTA',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- NTA/src/app/app-routing.module.ts
+++ NTA/src/app/app-routing.module.ts
@@ -5,10 +5,6 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./Login/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
