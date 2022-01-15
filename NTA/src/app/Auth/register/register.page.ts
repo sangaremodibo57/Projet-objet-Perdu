26e1ccf9f31659886878ec76bee9285e5e4b084a
Modibo Sangare <sangaremodibo57@gmail.com>
2022-01-15 22:18:31 +0000

    page register

--- NTA/src/app/Auth/register/register.page.ts
+++ NTA/src/app/Auth/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
-
+  todo = {
+    title: '',
+    description: ''
+  };
+  logForm(form) {
+    console.log(form.value)
+  }
   ngOnInit() {
   }
 
