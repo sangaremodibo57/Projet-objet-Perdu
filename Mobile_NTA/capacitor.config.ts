6a26b151a4544a59396098ffcb2bba090a29f2db
Modibo Sangare <sangaremodibo57@gmail.com>
2022-03-12 10:37:10 +0000

    new projet

---
+++ Mobile_NTA/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mobile_NTA',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
